fa3d4ee00454dbaa809b3f6822d12b9edca07760
yitinglei <yitinglei0207@gmail.com>
2015-04-20 10:54:27 +0800

    Initial Commit

---
+++ StaticCellPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StaticCellPractice
+//
+//  Created by Jason Lei on 2015/4/20.
+//  Copyright (c) 2015年 AlphaCamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StaticCellPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticCellPractice
+//
+//  Created by Jason Lei on 2015/4/20.
+//  Copyright (c) 2015年 AlphaCamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
